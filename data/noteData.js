06ece3da948905ddea8a6b665ffe80733c0b6e46
Israel Guillermo <israelguillermo22@gmail.com>
2020-11-25 23:24:03 -0600

    updating API routes

--- data/noteData.js
+++
@@ -1,7 +0,0 @@
-// const note = {
-//   title: "This is a Title",
-//   text: "This is a text",
-//   id: 0,
-// };
-
-// module.exports = note;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,20 @@
-const path = require("path");
 const router = require("express").Router();
 const fs = require("fs");
-const noteData = require("../data/noteData");
-const outputPath = path.join(__dirname, "../db/db.json");
+const noteData = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
-router.route("/notes").get((_req, res) => {
-  fs.readFile(outputPath, (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    res.json(notes);
+router
+  .route("/notes")
+  .get((_req, res) => {
+    res.json(noteData);
+  })
+  .post((req, res) => {
+    req.body.id = uuidv4();
+    noteData.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
+      if (err) throw err;
+    });
+    res.json(noteData);
   });
-});
 
 module.exports = router;
